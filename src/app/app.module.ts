819d104514b83fe7d6465e81b6c27f3beb977388
Tim <tja2353@gmail.com>
2019-10-03 12:59:33 +0100

    tidy AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
@@ -21,7 +22,6 @@ import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { metaReducers, ROOT_REDUCERS } from './reducers';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   imports: [
