d4a5abc47bb8d235a6d1d4eb2b9ad6d18c50c7c5
Tim <tja2353@gmail.com>
2019-10-09 11:10:48 +0100

    removed CurrentTaskListItemComponent

--- src/app/tasks/components/index.ts
+++ src/app/tasks/components/index.ts
@@ -6,4 +6,3 @@ export * from './task-list-detail-edit/task-list-detail-edit.component';
 export * from './task-list-detail-new/task-list-detail-new.component';
 export * from './task-list-list/task-list-list.component';
 export * from './current-task-detail-edit/current-task-detail-edit.component';
-export * from './current-task-list-item/current-task-list-item.component';
