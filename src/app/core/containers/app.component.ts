ac032f846921e17412f1beec37806798c3d85b0b
Tim <tja2353@gmail.com>
2019-10-13 14:45:25 +0100

    add service worker update check

--- src/app/core/containers/app.component.ts
+++ src/app/core/containers/app.component.ts
@@ -1,8 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 import { select, Store } from '@ngrx/store';
 
 import { Observable } from 'rxjs';
+
+import * as fromRoot from '@app/reducers';
 
 /*
 import { AuthActions } from '@example-app/auth/actions';
@@ -10,7 +13,6 @@ import * as fromAuth from '@example-app/auth/reducers';
 import { LayoutActions } from '@example-app/core/actions';
 
 */
-import * as fromRoot from '@app/reducers';
 
 @Component({
   selector: 'app-root',
@@ -20,16 +22,29 @@ import * as fromRoot from '@app/reducers';
   `,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showSidenav$: Observable<boolean>;
   loggedIn$: Observable<boolean>;
 
-  constructor(private store: Store<fromRoot.State>) {
+  constructor(
+    private store: Store<fromRoot.State>,
+    private swUpdate: SwUpdate
+  ) {
     /**
      * Selectors can be applied with the `select` operator which passes the state
      * tree to the provided selector
      */
     // this.showSidenav$ = this.store.pipe(select(fromRoot.getShowSidenav));
     // this.loggedIn$ = this.store.pipe(select(fromAuth.getLoggedIn));
+  }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version available. Load New Version?')) {
+          window.location.reload();
+        }
+      });
+    }
   }
 }
