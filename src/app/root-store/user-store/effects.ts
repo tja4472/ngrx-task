59d3f6b82d1b26259dbf0654e1556a3674d7dd19
Tim <tja2353@gmail.com>
2019-11-01 11:17:37 +0000

    convert UserStoreEffects to use createEffect

--- src/app/root-store/user-store/effects.ts
+++ src/app/root-store/user-store/effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 
 import { from, of } from 'rxjs';
@@ -12,51 +12,68 @@ import { UserInfoDataService } from '@app/services/user-info.data.service';
 import * as featureActions from './actions';
 import * as featureSelectors from './selectors';
 
+/* =======================================
+Improve typings of createEffect, help debugging
+https://github.com/ngrx/platform/issues/2192
+
+effect$ = createEffect(() => {
+  return this.actions$.pipe(
+    ...
+  );
+});
+======================================= */
 @Injectable()
 export class UserStoreEffects {
-  @Effect()
-  haveFirebaseUser$ = this.actions$.pipe(
-    ofType(AuthApiActions.haveFirebaseUser),
-    concatMap((firebaseUser) =>
-      from(this.userInfoDataService.getUserData(firebaseUser.uid)).pipe(
-        map((userInfo) =>
-          featureActions.setData({
-            user: {
-              id: firebaseUser.uid,
-              email: firebaseUser.email,
-              name: firebaseUser.displayName,
-            },
-            taskListId: userInfo.todoListId,
-          })
+  haveFirebaseUser$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(AuthApiActions.haveFirebaseUser),
+      concatMap((firebaseUser) =>
+        from(this.userInfoDataService.getUserData(firebaseUser.uid)).pipe(
+          map((userInfo) =>
+            featureActions.setData({
+              user: {
+                id: firebaseUser.uid,
+                email: firebaseUser.email,
+                name: firebaseUser.displayName,
+              },
+              taskListId: userInfo.todoListId,
+            })
+          )
         )
       )
-    )
-  );
+    );
+  });
 
-  @Effect()
-  setData$ = this.actions$.pipe(
-    ofType(featureActions.setData),
-    map(({ user }) => user),
-    switchMap((user) => [
-      featureActions.haveUser({
-        userId: user.id,
-      }),
-      AuthApiActions.haveAppUser(),
-    ])
-  );
-  @Effect({ dispatch: false })
-  setUserListId$ = this.actions$.pipe(
-    ofType(featureActions.setTaskListId),
-    concatMap((action) =>
-      of(action).pipe(
-        withLatestFrom(this.store.select(featureSelectors.selectUser))
-      )
-    ),
-    tap(([action, user]) => {
-      const saveUser = { ...user, todoListId: action.taskListId };
+  setData$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(featureActions.setData),
+      map(({ user }) => user),
+      switchMap((user) => [
+        featureActions.haveUser({
+          userId: user.id,
+        }),
+        AuthApiActions.haveAppUser(),
+      ])
+    );
+  });
 
-      this.userInfoDataService.save(saveUser, user.id);
-    })
+  setUserListId$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(featureActions.setTaskListId),
+        concatMap((action) =>
+          of(action).pipe(
+            withLatestFrom(this.store.select(featureSelectors.selectUser))
+          )
+        ),
+        tap(([action, user]) => {
+          const saveUser = { ...user, todoListId: action.taskListId };
+
+          this.userInfoDataService.save(saveUser, user.id);
+        })
+      );
+    },
+    { dispatch: false }
   );
 
   constructor(
