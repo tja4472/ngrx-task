1a820fe82f3744f5ca73fa066bfc36e51c71adf2
Tim <tja2353@gmail.com>
2025-01-08 11:10:42 +0000

    Remove enableProdMode()

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
-import { enableProdMode, inject, runInInjectionContext } from '@angular/core';
+import { inject, runInInjectionContext } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { environment } from './environments/environment';
 
 import { AppComponent } from '@app/core/containers/app.component';
 
 import { AppActionsService } from '@app/services/app-actions.service';
-
-if (environment.production) {
-  // TODO: Remove this?
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, appConfig)
   .then((appRef) => {
